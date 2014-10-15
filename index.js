98e5d3cd699356cb78cdb8b189e61b4fb894cecf
Julian Gruber <julian@juliangruber.com>
2014-10-15 15:20:59 +0200

    simplify a lot

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-var Readable = require('stream').Readable;
-var read = require('stream-read');
+var PassThrough = require('stream').PassThrough;
+var readonly = require('read-only-stream');
 
 module.exports = function cat(streams){
-  var idx = 0;
-  var ret = Readable();
-  ret._read = function(){
-    (function next(){
-      var stream = streams[idx];
-      if (!stream) return ret.push(null);
-      read(stream, function(err, data){
-        if (err) {
-          endPending();
-          return ret.emit('error', err);
-        }
-        if (!data) {
-          idx++;
-          next();
-        } else {
-          ret.push(data);
-        }
-      });
-    })();
-  };
-  function endPending(){
-    streams.slice(idx).forEach(function(stream){
-      stream.end();
+  var out = PassThrough();
+
+  (function next(i){
+    var stream = streams[i];
+    if (!stream) return out.push(null);
+    stream.pipe(out, { end: false });
+    stream.on('end', function(){
+      next(i + 1);
     });
-  }
-  return ret;
+  })(0);
+
+  return readonly(out);
 };
